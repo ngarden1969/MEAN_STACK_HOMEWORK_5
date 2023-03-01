4b916d0c8ef9a3157d37fc22a62cc3a09c6af942
Bhim Tamang <ngardenozzy@gmail.com>
2023-03-01 00:13:52 -0600

    immediately invoked function with closure to print messages

---
+++ index.js
@@ -0,0 +1,23 @@
+/* 1:Create a function that everytime you invoke it, it will print out the message "Congrats you earn the chance!", however it can only print out the message with the first 5 excutions. all the rest invoke will print out the message "Sorry you missed the chance"*/
+
+const printMsg = (function () {
+  let counter = 0;
+  return () => {
+    if (counter < 5) {
+      console.log("Congrats you earn the chance.");
+      counter += 1;
+      return counter;
+    } else {
+      console.log("Sorry! you missed the chance.");
+      counter += 1;
+      return counter;
+    }
+  };
+})();
+printMsg();
+printMsg();
+printMsg();
+printMsg();
+printMsg();
+printMsg();
+printMsg();
